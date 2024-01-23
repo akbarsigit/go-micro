7f1f2658779b19ba9dafbd4392d2a155a10a3640
akbarsigit <akbarsigit3@gmail.com>
2024-01-23 20:20:58 +0700

    setting up rabbit mq

---
+++ listener-service/event/event.go
@@ -0,0 +1,13 @@
+package event
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func declareExchange(ch *amqp.Channel) error {
+	return ch.ExchangeDeclare(
+		"logs_topic",
+		"topic",
+		
+	)
+}
